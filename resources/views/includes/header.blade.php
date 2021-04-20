8d15b7e0c39cb33fc6fb0ea21a0652bdfb4fa47a
Ernest Blaz <216727@edu.p.lodz.pl>
2021-04-20 19:12:09 +0200

    add layout

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>My app</title>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="{{ asset('js/scripts.js') }}"></script>
+<link rel="stylesheet" href="{{ asset('css/styles.css') }}">
